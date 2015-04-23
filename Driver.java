8d83d612e227d25382c6276fcad7188072fde238
Joel Vojta <joel.vojta@cuw.edu>
2015-04-22 22:19:19 -0500

    HW 15 Incomplete

--- Driver.java
+++ Driver.java
@@ -4,8 +4,9 @@ public class Driver
 	public static void main(String[] args)
 	{
 		Parser p = new Parser("     a =     (a *   b) - c;");
-		Parser p2 = new Parser("     a = b - c;");
+		//Parser p2 = new Parser("     a    = b - c;");
 
 		p.parse();
+		//p2.parse();
 	}
 }
--- Parser.java
+++ Parser.java
@@ -2,7 +2,7 @@ public class Parser
 {
 	private String theStmt;
 	private int pos; //where am I in the theStmt string
-	
+
 	public Parser(String theStmt)
 	{
 		this.theStmt = theStmt;
@@ -16,23 +16,69 @@ public class Parser
 	
 	private void parse_stmt()
 	{
+		theStmt = theStmt.replaceAll("\\s","");  //this will take away all the empty spaces, making it easier to navigate
+		String theVar = ""; //the first variable
+		
+		for (int i = this.pos; i < theStmt.indexOf("="); i++)  //in case the variable has multiple characters
+		{
+			theVar = theVar + theStmt.charAt(pos);
+			pos++;
+		}
+		pos++;
+		System.out.println("Read: Varname = " + theVar);
+		String mathExpr = theStmt.substring(pos);   //finds the expression and makes it into string
+		System.out.println("Reading Math Expr: " + mathExpr);
+		parse_math_expr();
+		}
+	
 		//Print each time it reads something like:
 		// Read: VarName = a
 		// Reading: Math-Expr
 		
 		//read a var name
 		//read a math_expr
-	}
 	
 	private void parse_math_expr()
 	{
-		//Display
-		//Reading Left:
-		//Read OP = *
-		//Reading Right
+		String inside = "";  //create a string for expressions within ()
+		String theLeft = "";
+		String theRight = "";
 		
-		//read left
-		//read op
-		//read right
+		//THIS IS THE PART WHERE THINGS MESS UP FOR ME
+		if(theStmt.indexOf("(") == pos) //is there another math expression here?
+		{
+			for(int i = pos + 1; i < theStmt.indexOf(")"); i++) //only pay attention to what's inside the ()
+			{
+				inside = inside + theStmt.charAt(i); //create the inside expression string
+				System.out.println(inside);  
+				pos++;  
+				parse_math_expr(); //restart loop with string inside
+			}
+		}
+		
+		//THIS PART SEEMS TO WORK OK, FOR THE MOST PART
+		else 
+		{
+			//theLeft variable is everything up to the next operator
+			for (int i = this.pos; i < theStmt.indexOf("*") || i < theStmt.indexOf("/") || i < theStmt.indexOf("+") || i < theStmt.indexOf("-") || i < theStmt.indexOf("%");  i++)  
+			{
+				theLeft = theLeft + theStmt.charAt(pos);
+				pos++;
+			}
+			System.out.println("Reading Left: " + theLeft);
+			System.out.println("Reading Operator: " + theStmt.charAt(pos));
+			pos++;
+			//the right variable ends before the ) or the ;
+			for (int i = this.pos; i < theStmt.indexOf(")") || i < theStmt.indexOf(";");  i++)  
+			{
+				theRight = theRight + theStmt.charAt(pos);
+				pos++;
+			}
+			System.out.println("Reading Right: " + theRight);
+		}
 	}
 }
+
+
+
+	
